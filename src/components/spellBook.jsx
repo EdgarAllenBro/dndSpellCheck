9c170997a72264a5be97c376d2dcf478a73cb5d2
CameronG <cjgrieder@gmail.com>
2023-12-07 16:45:31 -0700

    working on filter function

--- src/components/spellBook.jsx
+++ src/components/spellBook.jsx
@@ -2,25 +2,29 @@
 import { useNavigate } from "react-router-dom"
 import { useState, useEffect } from "react"
 import axios from "axios"
-import { redirect } from "react-router-dom"
 
 
 const SpellBook = ()=>{
 const [allSpells,setAllSpells] = useState()
-const [filterSpells ,setFilter] = useState()
+const [filterSpells ,setFilterSpells] = useState()
+const [filter,setFilter] = useState('')
 const redirect = useNavigate()
+
+const handleFilter = ()=>{
+    setFilterSpells()
+}
 
 useEffect(()=>{
     axios.get('https://www.dnd5eapi.co/api/spells').then((res)=>{
     setAllSpells(res.data.results)
-    setFilter(res.data.results)
+    setFilterSpells(res.data.results)
 })
 },[])
 
     return (
         <>
             <h1>Spell Book</h1>
-
+            <input onChange={(e)=>setFilter(e.target.value)} value={filter} />
             <div>{filterSpells ? filterSpells.map((spell)=>{
                 return <p onClick={()=>redirect('/spellDetails',{state:{index:spell.index,name:spell.name}})} className="listSpell" key={spell.index}>{spell.name}</p>
             }):''}</div>
